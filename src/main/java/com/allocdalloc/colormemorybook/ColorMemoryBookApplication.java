87c9ac9a48399871cee6df29880aa44e4388bf2f
mingeun0507 <tqdlfkg@naver.com>
2023-04-05 17:40:37 +0900

    initial commit

---
+++ src/main/java/com/allocdalloc/colormemorybook/ColorMemoryBookApplication.java
@@ -0,0 +1,13 @@
+package com.allocdalloc.colormemorybook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColorMemoryBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColorMemoryBookApplication.class, args);
+    }
+
+}
