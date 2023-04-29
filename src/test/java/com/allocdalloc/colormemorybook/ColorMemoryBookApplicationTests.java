3602717e0a780adadc2eca26ced15cb188f4593c
99winnmin <ojysep9987@gmail.com>
2023-04-29 20:23:59 +0900

    [Chore] sample test

--- src/test/java/com/allocdalloc/colormemorybook/ColorMemoryBookApplicationTests.java
+++ src/test/java/com/allocdalloc/colormemorybook/ColorMemoryBookApplicationTests.java
@@ -1,13 +1,13 @@
-package com.allocdalloc.colormemorybook;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ColorMemoryBookApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.allocdalloc.colormemorybook;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ColorMemoryBookApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
