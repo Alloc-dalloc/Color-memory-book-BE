c5bd6e30a78bcb067d332fe023f4ec8f534e5980
99winnmin <ojysep9987@gmail.com>
2023-04-30 21:23:50 +0900

    [Feat] apple 로그인 response dto

---
+++ src/main/java/com/allocdalloc/colormemorybook/dto/auth/LoginResponseDto.java
@@ -0,0 +1,27 @@
+package com.allocdalloc.colormemorybook.dto.auth;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LoginResponseDto {
+    private String accessToken;
+    private String refreshToken;
+
+    @Builder
+    public LoginResponseDto(String accessToken, String refreshToken) {
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
+
+    public static LoginResponseDto of(String accessToken, String refreshToken) {
+        return LoginResponseDto.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
+}
+
