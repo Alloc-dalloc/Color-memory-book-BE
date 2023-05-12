26a0365df9bfd1a3e5aee4d5d93b28d7ff452290
mingeun0507 <tqdlfkg@naver.com>
2023-05-12 14:22:53 +0900

    [Feat] 이미지 업로드 시 Label 정보 리턴하도록 구현

--- src/main/java/com/allocdalloc/colormemorybook/service/ImageService.java
+++ src/main/java/com/allocdalloc/colormemorybook/service/ImageService.java
@@ -9,10 +9,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.services.rekognition.RekognitionClient;
+import software.amazon.awssdk.services.rekognition.model.*;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 
 @RequiredArgsConstructor
@@ -20,9 +23,12 @@ import java.util.Optional;
 @Service
 public class ImageService {
     private final AmazonS3Client amazonS3Client;
+    private final RekognitionClient rekognitionClient;
+
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
+
 
     public ImageUploadResponseDto upload(MultipartFile multipartFile, String dirName) throws IOException {
         if (multipartFile == null || multipartFile.isEmpty()) {
@@ -40,13 +46,13 @@ public class ImageService {
 
         removeNewFile(uploadFile);  // 로컬에 생성된 File 삭제 (MultipartFile -> File 전환 하며 로컬에 파일 생성됨)
 
-        return ImageUploadResponseDto.from(uploadImageUrl);      // 업로드된 파일의 S3 URL 주소 반환
+        return ImageUploadResponseDto.from(uploadImageUrl, detectLabels(fileName));      // 업로드된 파일의 S3 URL 주소 반환
     }
 
     private String putS3(File uploadFile, String fileName) {
         amazonS3Client.putObject(
                 new PutObjectRequest(bucket, fileName, uploadFile)
-                        .withCannedAcl(CannedAccessControlList.PublicRead)	// PublicRead 권한으로 업로드 됨
+                        .withCannedAcl(CannedAccessControlList.PublicRead)    // PublicRead 권한으로 업로드 됨
         );
         return amazonS3Client.getUrl(bucket, fileName).toString();
     }
@@ -69,5 +75,26 @@ public class ImageService {
             return Optional.of(convertFile);
         }
         return Optional.empty();
+    }
+
+    private DetectLabelsResponse detectLabels(String fileName) {
+        S3Object s3Object = S3Object.builder()
+                .bucket(bucket)
+                .name(fileName)
+                .build();
+
+        Image image = Image.builder()
+                .s3Object(s3Object)
+                .build();
+
+        DetectLabelsRequest detectLabelsRequest = DetectLabelsRequest.builder()
+                .image(image)
+                .maxLabels(10)
+                .build();
+
+        DetectLabelsResponse response = rekognitionClient.detectLabels(detectLabelsRequest);
+        response.labels().stream().map(Label::toString).forEach(log::info);
+
+        return response;
     }
 }
