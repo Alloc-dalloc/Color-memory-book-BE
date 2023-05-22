34de2e37d53a40a00aca1a00ba8c7e65fcb783b2
99winnmin <ojysep9987@gmail.com>
2023-05-22 23:25:55 +0900

    [Fix] s3 uploader 함수 변경

--- src/main/java/com/allocdalloc/colormemorybook/service/ImageService.java
+++ src/main/java/com/allocdalloc/colormemorybook/service/ImageService.java
@@ -1,9 +1,7 @@
 package com.allocdalloc.colormemorybook.service;
 
 import com.allocdalloc.colormemorybook.dto.image.response.ImageUploadResponseDto;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.allocdalloc.colormemorybook.util.AmazonS3Uploader;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,69 +10,21 @@ import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.services.rekognition.RekognitionClient;
 import software.amazon.awssdk.services.rekognition.model.*;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.List;
-import java.util.Optional;
-
 @RequiredArgsConstructor
 @Slf4j
 @Service
 public class ImageService {
-    private final AmazonS3Client amazonS3Client;
     private final RekognitionClient rekognitionClient;
+    private final AmazonS3Uploader amazonS3Uploader;
 
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-
-    public ImageUploadResponseDto upload(MultipartFile multipartFile, String dirName) throws IOException {
-        if (multipartFile == null || multipartFile.isEmpty()) {
-            throw new IllegalArgumentException("MultipartFile이 비어있습니다.");
-        }
-
-        File uploadFile = convert(multipartFile)
-                .orElseThrow(() -> new IllegalArgumentException("MultipartFile -> File 전환 실패"));
-        return upload(uploadFile, dirName);
-    }
-
-    private ImageUploadResponseDto upload(File uploadFile, String dirName) {
-        String fileName = dirName + "/" + uploadFile.getName();
-        String uploadImageUrl = putS3(uploadFile, fileName);
-
-        removeNewFile(uploadFile);  // 로컬에 생성된 File 삭제 (MultipartFile -> File 전환 하며 로컬에 파일 생성됨)
-
-        return ImageUploadResponseDto.from(uploadImageUrl, detectLabels(fileName));      // 업로드된 파일의 S3 URL 주소 반환
-    }
-
-    private String putS3(File uploadFile, String fileName) {
-        amazonS3Client.putObject(
-                new PutObjectRequest(bucket, fileName, uploadFile)
-                        .withCannedAcl(CannedAccessControlList.PublicRead)    // PublicRead 권한으로 업로드 됨
-        );
-        return amazonS3Client.getUrl(bucket, fileName).toString();
-    }
-
-    private void removeNewFile(File targetFile) {
-        if(targetFile.delete()) {
-            log.info("파일이 삭제되었습니다.");
-        }else {
-            log.info("파일이 삭제되지 못했습니다.");
-        }
-    }
-
-    private Optional<File> convert(MultipartFile file) throws IOException {
-
-        File convertFile = new File(file.getOriginalFilename());
-        if(convertFile.createNewFile()) {
-            try (FileOutputStream fos = new FileOutputStream(convertFile)) {
-                fos.write(file.getBytes());
-            }
-            return Optional.of(convertFile);
-        }
-        return Optional.empty();
+    public ImageUploadResponseDto s3Upload(MultipartFile image) throws Exception {
+        String s3FileName = amazonS3Uploader.makeS3FileName(image);
+        String uploadImageUrl = amazonS3Uploader.saveFileAndGetUrl(image, s3FileName);
+        return ImageUploadResponseDto.from(uploadImageUrl, detectLabels(s3FileName));      // 업로드된 파일의 S3 URL 주소 반환
     }
 
     private DetectLabelsResponse detectLabels(String fileName) {
