5ba4e867af4868a987a6e87289c8706e24b3fde5
99winnmin <ojysep9987@gmail.com>
2023-05-24 16:15:13 +0900

    [Fix] 초기 사진 업로드 api 명 변경

--- src/main/java/com/allocdalloc/colormemorybook/controller/image/ImageController.java
+++ src/main/java/com/allocdalloc/colormemorybook/controller/image/ImageController.java
@@ -1,6 +1,6 @@
 package com.allocdalloc.colormemorybook.controller.image;
 
-import com.allocdalloc.colormemorybook.dto.image.response.ImageUploadResponseDto;
+import com.allocdalloc.colormemorybook.dto.image.response.ImageTotalAnalysisResponseDto;
 import com.allocdalloc.colormemorybook.service.ImageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,11 +16,11 @@ import org.springframework.web.multipart.MultipartFile;
 public class ImageController {
     private final ImageService imageService;
 
-    @PostMapping("/upload")
-    public ResponseEntity<ImageUploadResponseDto> uploadImage(
+    @PostMapping("/analysis")
+    public ResponseEntity<ImageTotalAnalysisResponseDto> uploadImage(
             @RequestPart(value = "image") MultipartFile multipartFile
             ) throws Exception {
 
-        return ResponseEntity.ok().body(imageService.s3Upload(multipartFile));
+        return ResponseEntity.ok().body(imageService.imageAnalysis(multipartFile));
     }
 }
