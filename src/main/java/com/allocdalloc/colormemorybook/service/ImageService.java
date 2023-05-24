dca444e377a07e1958cbecba1bb459ca9b0bf329
99winnmin <ojysep9987@gmail.com>
2023-05-24 16:16:34 +0900

    [Feat] 초기 사진 업로드 로직 완료 - Rekognition, Flask 연동 완료

--- src/main/java/com/allocdalloc/colormemorybook/service/ImageService.java
+++ src/main/java/com/allocdalloc/colormemorybook/service/ImageService.java
@@ -1,14 +1,23 @@
 package com.allocdalloc.colormemorybook.service;
 
-import com.allocdalloc.colormemorybook.dto.image.response.ImageUploadResponseDto;
+import com.allocdalloc.colormemorybook.dto.image.response.ImageColorAnalysisResponseDto;
+import com.allocdalloc.colormemorybook.dto.image.response.ImageTotalAnalysisResponseDto;
 import com.allocdalloc.colormemorybook.util.AmazonS3Uploader;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.services.rekognition.RekognitionClient;
 import software.amazon.awssdk.services.rekognition.model.*;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -16,15 +25,16 @@ import software.amazon.awssdk.services.rekognition.model.*;
 public class ImageService {
     private final RekognitionClient rekognitionClient;
     private final AmazonS3Uploader amazonS3Uploader;
+    private final RestTemplate restTemplate;
 
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-    public ImageUploadResponseDto s3Upload(MultipartFile image) throws Exception {
+    public ImageTotalAnalysisResponseDto imageAnalysis(MultipartFile image) throws Exception {
         String s3FileName = amazonS3Uploader.makeS3FileName(image);
         String uploadImageUrl = amazonS3Uploader.saveFileAndGetUrl(image, s3FileName);
-        return ImageUploadResponseDto.from(uploadImageUrl, detectLabels(s3FileName));      // 업로드된 파일의 S3 URL 주소 반환
+        return ImageTotalAnalysisResponseDto.from(uploadImageUrl, detectLabels(s3FileName), analysisColor(uploadImageUrl));      // 업로드된 파일의 S3 URL 주소 반환
     }
 
     private DetectLabelsResponse detectLabels(String fileName) {
@@ -47,4 +57,27 @@ public class ImageService {
 
         return response;
     }
+
+    private List<ImageColorAnalysisResponseDto> analysisColor(String fileName) {
+        // Request Header 설정
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+        // Request Body 설정
+        Map<String, Object> requestBody = new HashMap<>();
+        requestBody.put("image", fileName);
+
+
+        HttpEntity<Map<String, Object>> request = new HttpEntity<>(requestBody, headers);
+
+        ResponseEntity<List<ImageColorAnalysisResponseDto>> response = restTemplate.exchange(
+                "/color",
+                HttpMethod.POST,
+                request,
+                new ParameterizedTypeReference<List<ImageColorAnalysisResponseDto>>() {}
+        );
+        return response.getBody();
+    }
+
 }
