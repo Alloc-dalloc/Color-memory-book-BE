173d286059a6dccc732769498aae057a83709514
99winnmin <ojysep9987@gmail.com>
2023-05-24 19:58:42 +0900

    [Feat] 컬매북 등록, home 조회 및 검색 api 구현

---
+++ src/main/java/com/allocdalloc/colormemorybook/controller/colorMemoryBook/ColorMemoryBookController.java
@@ -0,0 +1,47 @@
+package com.allocdalloc.colormemorybook.controller.colorMemoryBook;
+
+import com.allocdalloc.colormemorybook.dto.colorMemoryBook.request.ColorMemoryBookRegisterRequestDto;
+import com.allocdalloc.colormemorybook.dto.colorMemoryBook.response.ColorMemoryBookHomeResponse;
+import com.allocdalloc.colormemorybook.dto.colorMemoryBook.response.ColorMemoryBookRegisterResponseDto;
+import com.allocdalloc.colormemorybook.entity.user.detail.UserAccount;
+import com.allocdalloc.colormemorybook.service.ColorMemoryBookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/color-memory-book")
+@RequiredArgsConstructor
+public class ColorMemoryBookController {
+    private final ColorMemoryBookService colorMemoryBookService;
+
+    @PostMapping("/register")
+    public ResponseEntity<ColorMemoryBookRegisterResponseDto> registerColorMemoryBook
+            (
+                    @AuthenticationPrincipal UserAccount userAccount,
+                    @RequestBody ColorMemoryBookRegisterRequestDto colorMemoryBookRegisterRequestDto
+            )
+    {
+        ColorMemoryBookRegisterResponseDto colorMemoryBookRegisterResponseDto = colorMemoryBookService.registerColorMemoryBook(userAccount, colorMemoryBookRegisterRequestDto);
+        return ResponseEntity.ok().body(colorMemoryBookRegisterResponseDto);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<ColorMemoryBookHomeResponse>> home
+            (
+                    @AuthenticationPrincipal UserAccount userAccount,
+                    @Nullable @RequestParam Long cursor,
+                    @PageableDefault(size = 6) Pageable pageable,
+                    @RequestParam(required = false, defaultValue = "") String keyword
+            )
+    {
+        List<ColorMemoryBookHomeResponse> materialAtHome = colorMemoryBookService.getMaterialAtHome(userAccount, cursor, pageable, keyword);
+        return ResponseEntity.ok().body(materialAtHome);
+    }
+}
