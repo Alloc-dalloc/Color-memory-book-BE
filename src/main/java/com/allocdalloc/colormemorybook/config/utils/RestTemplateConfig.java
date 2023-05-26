605fdf926bf37e8d9a15ab3499e2ff9aae31b091
99winnmin <ojysep9987@gmail.com>
2023-05-26 16:09:24 +0900

    [Fix] Flask 호출부 변경

--- src/main/java/com/allocdalloc/colormemorybook/config/utils/RestTemplateConfig.java
+++ src/main/java/com/allocdalloc/colormemorybook/config/utils/RestTemplateConfig.java
@@ -25,7 +25,7 @@ public class RestTemplateConfig {
                 .setConnectTimeout(Duration.ofMillis(50000)) // connection-timeout
                 .setReadTimeout(Duration.ofMillis(50000)) // read-timeout
                 .additionalMessageConverters(new StringHttpMessageConverter(StandardCharsets.UTF_8))
-                .rootUri("http://localhost:5000")
+                .rootUri("http://127.0.0.1:5000")
                 .build();
     }
 
